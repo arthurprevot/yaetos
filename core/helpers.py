77cb343fa2d66f4b8425becaa45d04761b75031c
Arthur Prevot <arthur.prevot@scmspain.com>
2017-09-18 21:17:34 +0200

    move scheduling data out of helpers.py

--- core/helpers.py
+++ core/helpers.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 # from core.run import DeployPySparkScriptOnAws
-
+from conf.scheduling import schedule_local as schedule  # TODO for testing
 
 class etl():
     def run(self, sc, **kwargs):
@@ -12,15 +12,3 @@ class etl():
         output = self.run(sc, **new_args)
         output.saveAsTextFile(schedule['wordcount']['output'])
         return output
-
-
-schedule = {
-    'wordcount':{
-        'inputs': [
-            ['lines',"s3://bucket-scratch/wordcount_test/input/sample_text.txt"],
-            # ['lines',"sample_text.txt"],  # local
-        ],
-        'output': 's3://bucket-scratch/wordcount_test/output/v4/'
-        # 'output': 'tmp/output_v5/'  # local
-    }
-}
