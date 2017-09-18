69f393a5cdd94b9687d3bec95d00566b8ce377d0
Arthur Prevot <arthur.prevot@scmspain.com>
2017-09-19 00:10:21 +0200

    cleaning

--- core/helpers.py
+++ core/helpers.py
@@ -1,13 +1,8 @@
-from pyspark import SparkContext
-# from conf.scheduling import schedule_local as schedule  # TODO for testing
-
 class etl():
     def run(self, sc, **kwargs):
         raise NotImplementedError
 
     def runner(self, sc):
-        # import ipdb; ipdb.set_trace()
-
         new_args = {}
         for item in self.INPUTS.keys():
             new_args[item] = sc.textFile(self.INPUTS[item]['path'])
@@ -23,16 +18,11 @@ def launch(classname, appName, app_file, aws):
     process = 'run_local'
     if len(sys.argv) > 1:
         process = sys.argv[1]
-    # data_location = sys.argv[2] # can be local or cluster.
 
-    # sys.exit()
     if process == 'run_local':
         from pyspark import SparkContext
-        # sc = SparkContext(appName="PythonWordCount")
         sc = SparkContext(appName=appName)
-        # wordcount().runner(sc)
         classname().runner(sc)
     elif process == 'ship_cluster':
         from core.run import DeployPySparkScriptOnAws
-        # DeployPySparkScriptOnAws(app_file="jobs/spark_example/wordcount_frameworked.py", setup='perso').run()
         DeployPySparkScriptOnAws(app_file=app_file, setup=aws).run()
