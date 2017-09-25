f2c75575620bc6ea6f20d770f16b1eb3c98695fc
Arthur Prevot <arthur.prevot@scmspain.com>
2017-09-25 18:38:42 +0200

    cleaning

--- core/sql_generic_job.py
+++ core/sql_generic_job.py
@@ -4,16 +4,16 @@ from core.etl_utils import etl, launch
 class generic(etl):
 
     def run(self, some_events, other_events, sql_file):
-        sql = read_sql_file(sql_file)
+        sql = self.read_sql_file(sql_file)
         tb = self.query(sql)
         return tb
 
-
-def read_sql_file(fname):
-    fh = open(fname, 'r')
-    sql = fh.read()
-    fh.close()
-    return sql
+    @staticmethod
+    def read_sql_file(fname):
+        fh = open(fname, 'r')
+        sql = fh.read()
+        fh.close()
+        return sql
 
 
 if __name__ == "__main__":
