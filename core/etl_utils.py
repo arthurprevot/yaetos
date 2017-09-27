75e0e5b835ce3a89d3079e0fa634474dfd436704
Arthur Prevot <arthur.prevot@scmspain.com>
2017-09-27 18:07:09 +0200

    refactor to isolate code that needs to know if run locally or on cluster, cleaner way to run instance

--- core/etl_utils.py
+++ core/etl_utils.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import os
 import boto3
 import argparse
+from time import time
 
 
 JOBS_METADATA_FILE = 'conf/jobs_metadata.yml'
@@ -19,21 +20,29 @@ class etl(object):
     def run(self, **app_args):
         raise NotImplementedError
 
-    def runner(self, sc, sc_sql, **app_args):
+    def run_handler(self, sc, sc_sql, location, **app_args):
+        # start_time = time()
         self.sc = sc
         self.sc_sql = sc_sql
+        self.location = location
+        self.app_name = sc.appName
+        self.set_path()
 
         loaded_datasets = self.load_inputs()
         app_args.update(loaded_datasets)
         output = self.run(**app_args)
         self.save(output)
+
+        # end_time = time()
+        # elapsed = end_time - start_time
+        # self.save_metadata()
         return output
 
-    def set_path(self, fname_schedule, app_name):
-        """Can override this method to force paths regardless of job_meta file."""
-        yml = self.load_schedule(fname_schedule)
-        self.INPUTS = yml[app_name]['inputs']  # TODO: add error handling to deal with KeyError when name not found in jobs_metadata.
-        self.OUTPUT = yml[app_name]['output']
+    def set_path(self):
+        meta_file = CLUSTER_APP_FOLDER+JOBS_METADATA_FILE if self.location=='cluster' else JOBS_METADATA_LOCAL_FILE
+        yml = self.load_meta(meta_file)
+        self.INPUTS = yml[self.app_name]['inputs']  # TODO: add error handling to deal with KeyError when name not found in jobs_metadata.
+        self.OUTPUT = yml[self.app_name]['output']
 
     def load_inputs(self):
         app_args = {}
@@ -71,13 +80,30 @@ class etl(object):
 
         print 'Wrote output to ',path
 
+    # def save_metadata(self):
+    #     return self.save_metadata_cluster() if self.location=='cluster' else self.save_metadata_local()
+    #
+    # def save_metadata_local(self):
+    #     content = "-- name: %s\n-- db_creds (#db_type#): %s\n-- time (s): %s\n-- query: \n%s\n-- end"%(name, db_type, elapsed, query_str)
+    #     write_file(fname_sql, content)
+    #
+    # def save_metadata_cluster(self):
+    #     content = "-- name: %s\n-- db_creds (#db_type#): %s\n-- time (s): %s\n-- query: \n%s\n-- end"%(name, db_type, elapsed, query_str)
+    #     write_file(fname_sql, content)
+
     def listdir(self, path):
+        return self.listdir_cluster(path) if self.location=='cluster' else self.listdir_local(path)
+
+    @staticmethod
+    def listdir_local(path):
         # TODO: make function clearer
         # For local path
-        if not path.lower().startswith('s3://'):
-            return os.listdir(path)
+        # if not path.lower().startswith('s3://'):  # TODO: use boolean set upstream to decide on local vs cluster.
+            # return os.listdir(path)
+        return os.listdir(path)
 
-        # For s3 path
+    @staticmethod
+    def listdir_cluster(path):
         bucket_name = path.split('s3://')[1].split('/')[0]
         prefix = '/'.join(path.split('s3://')[1].split('/')[1:])
         client = boto3.client('s3')
@@ -89,10 +115,17 @@ class etl(object):
         print 'Query string:', query_str
         return self.sc_sql.sql(query_str)
 
-    def load_schedule(self, fname):
+    @staticmethod
+    def load_meta(fname):
         with open(fname, 'r') as stream:
             yml = yaml.load(stream)
         return yml
+
+
+# def write_file(fname, content):
+#     fh = open(fname, 'w')
+#     fh.write(content)
+#     fh.close()
 
 
 def launch(job_class, sql_job=False, **kwargs):
@@ -102,7 +135,7 @@ def launch(job_class, sql_job=False, **kwargs):
     The inputs should not be dependent on whether the job is run locally or deployed to cluster as it is used for both.
     """
     # TODO: redo this function to clarify commandline args vs function args vs args to go into deploy or run mode.. could use kwargs to set params below if not overriden by commandline args.
-    # TODO: look at adding input and output path as cmdline as a way to override schedule ones.
+    # TODO: look at adding input and output path as cmdline as a way to override schedule ones. or better differentiate cmdline args vs app_args
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--execution", default='run', help="choose 'run' (default) or 'deploy_and_run'.", choices=set(['deploy_and_run', 'run'])) # comes from cmd line since value is set when running on cluster
@@ -116,7 +149,7 @@ def launch(job_class, sql_job=False, **kwargs):
 
     app_args = {}
     if sql_job and args.sql_file is not None:
-        app_args['sql_file']= args.sql_file  # TODO: add app_name there
+        app_args['sql_file']= args.sql_file  # TODO: add app_name and meta_file args there
 
     if args.execution == 'run':
         from pyspark import SparkContext
@@ -124,10 +157,7 @@ def launch(job_class, sql_job=False, **kwargs):
         app_name = job_class.__name__ if not sql_job else app_args['sql_file'].split('/')[-1].replace('.sql','')  # Quick and dirty, forces name of sql file to match schedule entry
         sc = SparkContext(appName=app_name)
         sc_sql = SQLContext(sc)
-        meta_file = CLUSTER_APP_FOLDER+JOBS_METADATA_FILE if args.location=='cluster' else JOBS_METADATA_LOCAL_FILE
-        myjob = job_class()
-        myjob.set_path(meta_file, app_name)
-        myjob.runner(sc, sc_sql, **app_args)
+        job_class().run_handler(sc, sc_sql, args.location, **app_args)
     elif args.execution == 'deploy_and_run':
         from core.deploy import DeployPySparkScriptOnAws
         aws_setup = kwargs.get('aws_setup', 'dev')
