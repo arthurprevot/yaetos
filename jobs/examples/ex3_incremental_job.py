af3193da89c40a9f5bfa7e94005ae1b207a1f339
Arthur Prevot <arthur.prevot@scmspain.com>
2017-11-21 09:24:41 +0100

    original ex3_incremental_prep_job.py now working

--- jobs/examples/ex3_incremental_job.py
+++ jobs/examples/ex3_incremental_job.py
@@ -3,7 +3,6 @@ from core.etl_utils import ETL_Base
 
 class Job(ETL_Base):
     def transform(self, processed_events):
-        # import ipdb; ipdb.set_trace()
         df = self.query("""
             SELECT timestamp_obj as other_timestamp, *
             FROM processed_events se
@@ -23,14 +22,15 @@ if __name__ == "__main__":
     sc_sql = SQLContext(sc)
     args = {'storage':'local'}
 
-    from jobs.examples.ex3_dependant_job import Job as Ex3_Dependant_Job
-    ex3_dependant_job = Ex3_Dependant_Job()
-    ex3_incremental_job = Job()
-    # app_name = 'ex3_dependant_job'
-    dep = ex3_dependant_job.etl(sc, sc_sql, args)
-    # app_name = 'ex3_incremental_job'
-    loaded_inputs = {'processed_events':dep}
-    out = ex3_incremental_job.etl(sc, sc_sql, args, loaded_inputs)
+    ## approach 1, manually
+    # from jobs.examples.ex3_dependant_job import Job as Ex3_Dependant_Job
+    # ex3_dependant_job = Ex3_Dependant_Job()
+    # ex3_incremental_job = Job()
+    # # app_name = 'ex3_dependant_job'
+    # dep = ex3_dependant_job.etl(sc, sc_sql, args)
+    # # app_name = 'ex3_incremental_job'
+    # loaded_inputs = {'processed_events':dep}
+    # out = ex3_incremental_job.etl(sc, sc_sql, args, loaded_inputs)
 
     # Notes
     # - outputs of inc jobs may have to be updated to have full table as output.
@@ -38,3 +38,15 @@ if __name__ == "__main__":
     # - need to handle sql jobs.
     # - could use networkx to build dependency of jobs.
     # -
+
+    ## approach 2, manually, both loaded externally
+    # from jobs.examples.ex3_dependant_job import Job as Ex3_Dependant_Job
+    # from jobs.examples.ex3_incremental_job import Job as Ex3_Incremental_Job
+    # dep = ex3_dependant_job.etl(sc, sc_sql, args)
+    # loaded_inputs = {'processed_events':dep}
+    # out = ex3_incremental_job.etl(sc, sc_sql, args, loaded_inputs)
+    #-> works
+
+    ## approach 3, using new class
+    from core.etl_utils import Flow
+    myflow = Flow(sc, sc_sql, args, 'examples/ex3_incremental_job')
