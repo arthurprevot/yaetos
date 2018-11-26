6e0510991dfa92e21cea71beebbcbafd378f8cde
Arthur Prevot <arthur.prevot@scmspain.com>
2018-11-27 00:09:50 +0100

    Update launch_env.sh

--- launch_env.sh
+++ launch_env.sh
@@ -4,4 +4,5 @@ docker run -it -p 4040:4040 -p 8080:8080 -p 8081:8081 \
     -v ~/.aws:/root/.aws \
     -h spark \
     pyspark_container
-    # you can remove "-v ~/.aws:/root/.aws" if you don't intend sending jobs to AWS.
+    # replace ~/code/pyspark_aws_etl by location of your pyspark_aws_etl repo.
+    # you can remove ~/.aws:/root/.aws if you don't intend sending jobs to AWS.
