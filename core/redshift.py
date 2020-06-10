520db7716152db440724c9d09ae3eef860b0188e
Arthur Prévot <aprevot@thehotelsnetwork.com>
2020-06-10 15:50:28 +0200

    logs

--- core/redshift.py
+++ core/redshift.py
@@ -13,10 +13,10 @@ def create_table(df, connection_profile, name_tb, schema, types, creds_or_file, 
     if_exist = 'replace' if not is_incremental else 'append'
     connection = connect(db=connection_profile, creds_or_file=creds_or_file)
     chunksize = 500000
-    logger.info('Sending table "{}" to redshift, mode "{}", size "{}", and chunksize "{}".'.format(name_tb, if_exist, len(df), chunksize))
+    logger.info('Sending table "{}" to redshift in schema "{}", mode "{}", size "{}", and chunksize "{}".'.format(name_tb, schema, if_exist, len(df), chunksize))
     df.to_sql(name=name_tb, schema=schema, con=connection, if_exists=if_exist, dtype=types, index=False, chunksize=chunksize)
     # TODO: check df.to_sql above for long integers. Noticed long numbers where rounded.
-    logger.info("Copied table to redshift '{}', using connection profile '{}'".format(name_tb, connection_profile))
+    logger.info("Copied table to redshift '{}.{}', using connection profile '{}'".format(schema, name_tb, connection_profile))
 
 
 logger = log.setup_logging('Redshift')
