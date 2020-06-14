2832ff11921fe87a9e8ecde1d66e7bd031dba67c
Arthur Prévot <aprevot@thehotelsnetwork.com>
2020-06-14 23:41:38 +0200

    make query_msql() compatible with EMR mode.

--- libs/python_db_connectors/query_mysql.py
+++ libs/python_db_connectors/query_mysql.py
@@ -4,9 +4,12 @@ import os
 from sqlalchemy import create_engine
 # requires pymysql installed.
 
-def connect(db):
+def connect(db, creds_or_file='conf/connections.cfg'):
     config = ConfigParser()
-    config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'credentials.cfg'))
+    if isinstance(creds_or_file, str):
+        config.read(creds_or_file)
+    else:
+        config = creds_or_file
 
     params = {
         'user':     config.get(db, 'user'),
