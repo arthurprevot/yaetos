336e15a4f06009948b6a9af7cb6fb2f630f9363a
Arthur <arthrprevot@gmail.com>
2020-09-20 22:34:05 +0200

    more cleaning

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,6 @@
 # Setup for run in local mode
+# Not needed when running from docker since included there.
 # Assumes spark (2.1.0) is already setup locally or in docker image
-
-
-#apt install software-properties-common
-#add-apt-repository ppa:deadsnakes/ppa
-#apt install python3.7 # doesn't work.
 
 mkdir -p tmp/files_to_ship/
 pip3 install -r requirements.txt
@@ -14,5 +10,3 @@ pip3 install -r requirements.txt
 export PYSPARK_AWS_ETL_HOME=`pwd`'/'
 export PYTHONPATH=$PYSPARK_AWS_ETL_HOME:$PYTHONPATH
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
-
-#mkdir -p data
