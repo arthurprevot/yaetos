4da2cf518a1d9b154eeb19196a97bfff9d332b4b
Arthur Prévot <aprevot@thehotelsnetwork.com>
2020-09-30 18:45:27 +0200

    salesforce connection now with token

--- libs/python_db_connectors/query_salesforce.py
+++ libs/python_db_connectors/query_salesforce.py
@@ -14,7 +14,10 @@ def connect(creds_section, creds_or_file='conf/connections.cfg'):
 
     user = config.get(creds_section, 'user')
     pwd = config.get(creds_section, 'password')
-    return Salesforce(username=user, password=pwd, security_token='')
+    token = config.get(creds_section, 'token')
+    # import ipdb; ipdb.set_trace()
+    # return Salesforce(username=user, password=pwd, security_token='', domain='poc1')
+    return Salesforce(username=user, password=pwd, security_token=token)
 
 def query(query_str, **connect_args):
     sf = connect(**connect_args)
