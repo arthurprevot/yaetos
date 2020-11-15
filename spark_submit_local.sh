71d511bfe3e0e9dd1e7b131bf445603db331d703
Arthur Prévot <aprevot@thehotelsnetwork.com>
2020-11-15 22:10:02 +0100

    cleaning

--- spark_submit_local.sh
+++ spark_submit_local.sh
@@ -7,12 +7,12 @@ job=$1
 export AWS_ACCESS_KEY_ID=`aws configure get default.aws_access_key_id`
 export AWS_SECRET_ACCESS_KEY=`aws configure get default.aws_secret_access_key`
 # Use `aws configure get aws_access_key_id --profile <new_profile>` to use different profile.
+packages=com.amazonaws:aws-java-sdk-pom:1.11.760,org.apache.hadoop:hadoop-aws:2.7.0,com.databricks:spark-redshift_2.11:2.0.1,org.apache.spark:spark-avro_2.11:2.4.0,mysql:mysql-connector-java:8.0.11  # should be in synch with list in deploy.py
+jars=https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.41.1065/RedshiftJDBC42-no-awssdk-1.2.41.1065.jar  # should be in synch with list in deploy.py
+
 spark-submit \
-  --packages com.amazonaws:aws-java-sdk-pom:1.11.760,org.apache.hadoop:hadoop-aws:2.7.0,com.databricks:spark-redshift_2.11:2.0.1,org.apache.spark:spark-avro_2.11:2.4.0,mysql:mysql-connector-java:8.0.11 \
-  --jars https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.41.1065/RedshiftJDBC42-no-awssdk-1.2.41.1065.jar \
+  --packages $packages \
+  --jars $jars \
   $job
-
-  # --packages com.amazonaws:aws-java-sdk-pom:1.11.760,org.apache.hadoop:hadoop-aws:2.7.0,com.databricks:spark-redshift_2.11:2.0.1,org.apache.spark:spark-avro_2.11:2.4.0,mysql:mysql-connector-java:5.1.39 \
-
 
 # TODO: check way to get arguments (ex: --storage=s3 --job_param_file=conf/jobs_metadata.yml) recognized
