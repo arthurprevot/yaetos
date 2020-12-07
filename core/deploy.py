783fcc8b51738b062b56e65bf4fe1db78567fbbd
Arthur Prévot <aprevot@thehotelsnetwork.com>
2020-12-07 16:14:47 +0100

    moved args to deploy_args instead of app_args. Tested in EMR_Scheduled. Prev commit tested in local and EMR

--- core/deploy.py
+++ core/deploy.py
@@ -460,11 +460,11 @@ class DeployPySparkScriptOnAws(object):
         # TODO: check if easier/simpler to change values at the source json instead of a processed one.
         # Change key pair
         myScheduleType = {'EMR_Scheduled': 'cron', 'EMR_DataPipeTest': 'ONDEMAND'}[self.app_args.get('mode')]
-        myPeriod = self.app_args['frequency'] or '1 Day'
-        if self.app_args['start_date'] and isinstance(self.app_args['start_date'], datetime):
-            myStartDateTime = self.app_args['start_date'].strftime('%Y-%m-%dT%H:%M:%S')
-        elif self.app_args['start_date'] and isinstance(self.app_args['start_date'], str):
-            myStartDateTime = self.app_args['start_date'].format(today=datetime.today().strftime('%Y-%m-%d'))
+        myPeriod = self.deploy_args['frequency'] or '1 Day'
+        if self.deploy_args['start_date'] and isinstance(self.deploy_args['start_date'], datetime):
+            myStartDateTime = self.deploy_args['start_date'].strftime('%Y-%m-%dT%H:%M:%S')
+        elif self.deploy_args['start_date'] and isinstance(self.deploy_args['start_date'], str):
+            myStartDateTime = self.deploy_args['start_date'].format(today=datetime.today().strftime('%Y-%m-%d'))
         else :
             myStartDateTime = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
         # TODO: self.app_args['start_date'] is taken from jobs_metadata_local.yml, instead of jobs_metadata.yml. Fix it
