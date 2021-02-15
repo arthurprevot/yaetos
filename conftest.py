4ab0dcebfd6df2fcef9a3125d6a73bb006603941
Arthur Prévot <aprevot@thehotelsnetwork.com>
2021-02-15 22:38:27 +0100

    cleaner test

--- conftest.py
+++ conftest.py
@@ -15,3 +15,17 @@ def sc_sql(sc):
 @pytest.fixture(scope="session")
 def ss(sc):  # TODO: check if sc necessary here since not used downstream
     return SparkSession.builder.appName('test').getOrCreate()
+
+@pytest.fixture
+def get_pre_jargs():
+    def internal_function(loaded_inputs):
+        inputs  = {key: {'type':None} for key in loaded_inputs.keys()}
+        pre_jargs = {
+            'defaults_args': {
+                'manage_git_info': False, 'mode':'dev_local', 'deploy':'none', 'job_param_file':None,
+                'output': {'type':None},
+                'inputs': inputs},
+            'job_args':{},
+            'cmd_args':{}}
+        return pre_jargs
+    return internal_function
--- tests/jobs/examples/ex1_frameworked_job_test.py
+++ tests/jobs/examples/ex1_frameworked_job_test.py
@@ -2,19 +2,8 @@ import pytest
 from jobs.examples.ex1_frameworked_job import Job
 
 
-def get_pre_jargs(loaded_inputs):
-    inputs  = {key: {'type':None} for key in loaded_inputs.keys()}
-    pre_jargs = {
-        'defaults_args': {
-            'manage_git_info': False, 'mode':'dev_local', 'deploy':'none', 'job_param_file':None,
-            'output': {'type':None},
-            'inputs': inputs},
-        'job_args':{},
-        'cmd_args':{}}
-    return pre_jargs
-
 class Test_Job(object):
-    def test_transform(self, sc, sc_sql, ss):
+    def test_transform(self, sc, sc_sql, ss, get_pre_jargs):
         some_events = ss.read.json(sc.parallelize([
             {'session_id': 1234, 'action': 'searchResultPage', 'n_results': 1},
             {'session_id': 1234, 'action': 'searchResultPage', 'n_results': 1},
