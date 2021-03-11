f9d1943180bf025219fb7ae853bbc188c50ba28b
Arthur Prévot <aprevot@thehotelsnetwork.com>
2021-03-11 18:18:31 +0100

    better fix

--- core/daily_incremental_job.py
+++ core/daily_incremental_job.py
@@ -19,8 +19,8 @@ class ETL_Daily_Incremental_Base(ETL_Base):
         """ Incremental assumes last available month from the previous output was fully loaded."""
         first_day = self.jargs.merged_args['first_day']
         if not self.last_attempted_period:
-            previous_day = self.get_previous_output_max_timestamp().strftime("%Y-%m-%d") if self.get_previous_output_max_timestamp() else None
-            self.last_attempted_period  = previous_day or first_day  # TODO: if get_output_max_timestamp()=None, means new build, so should delete instance in oracle.
+            previous_output_max_timestamp = self.get_previous_output_max_timestamp()
+            self.last_attempted_period  = previous_output_max_timestamp.strftime("%Y-%m-%d") if previous_output_max_timestamp else first_day  # TODO: if get_output_max_timestamp()=None, means new build, so should delete instance in DBs.
 
         periods = self.get_last_output_to_last_day(self.last_attempted_period, first_day)
         if len(periods) == 0:
