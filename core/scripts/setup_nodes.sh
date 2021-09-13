c7737c72e6977167e2472cfd3a1614bc7122f90b
Arthur Prévot <aprevot@thehotelsnetwork.com>
2021-09-13 22:43:46 +0200

    update AWS setup too.

--- core/scripts/setup_nodes.sh
+++ core/scripts/setup_nodes.sh
@@ -3,7 +3,7 @@
 
 # TODO: reuse "requirement.txt" libs but just the part that need to run on the cluster side
 # Installs below are specific to python version, which is specific to EMR version. TODO: make it independant. Will be fixed when using to emr-6.x since python3 will be default and it will support pip3.
-sudo pip-3.6 install boto3==1.9.57
+sudo pip-3.6 install boto3==1.18.41
 sudo pip-3.6 install networkx==2.4
 sudo pip-3.6 install numpy==1.18.5  # need to force this version instead of latest (1.19.2) to be compatible with koalas 1.3.0 (requiring <1.19)
 sudo pip-3.6 install pandas==1.0.0  # downgraded from 1.0.4 to be compatible with koalas. 0.25.1
