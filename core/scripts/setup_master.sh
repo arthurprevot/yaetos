3f796c58460067d5d866c2dd304c64eee32a5478
Arthur Prévot <aprevot@thehotelsnetwork.com>
2021-09-15 00:36:33 +0200

    update avro jar to 3.1.1 + more pip install to install cryptography properly but this part not working.

--- core/scripts/setup_master.sh
+++ core/scripts/setup_master.sh
@@ -9,15 +9,18 @@ s3_bucket_scripts="$s3_bucket/scripts.tar.gz"
 
 # Update awscli here as required by "aws s3 cp ..."
 # TODO: pip install -r requirements.txt # see related note in setup_nodes.sh
+#RUN apt-get update && apt-get install -y git
+#sudo pip3 install --upgrade pip
 sudo pip3 install awscli==1.20.41 # depends on botocore from 1.12.57
 sudo pip3 install scikit-learn==0.20.0  # TODO: remove when using req file
 sudo pip3 install statsmodels==0.9.0  # TODO: remove when using req file
 sudo pip3 install kafka-python==1.4.7
 sudo pip3 install jsonschema==3.0.2
-# sudo pip-3.6 install koalas==1.3.0  # fails installing now. TODO: check.
+# sudo pip3 install koalas==1.3.0  # fails installing now. TODO: check.
 # DB and API libs
 sudo pip3 install soql==1.0.2  # Necessary for simple-salesforce
-# sudo pip-3.6 install setuptools-rust==0.11.6  # Necessary for simple-salesforce
+sudo pip3 install wheel==0.37.0 # necessary for cryptography
+sudo pip3 install setuptools-rust==0.12.1  # Necessary for simple-salesforce
 sudo pip3 install cryptography==3.4.8  # Necessary for simple-salesforce, to avoid simple-salesforce loading 3.4.4 causing pbs.
 sudo pip3 install simple-salesforce==1.10.1
 sudo pip3 install pymysql==0.9.3
