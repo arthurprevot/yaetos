46a2b61d67ae59c9960e218790ccd5ad43305324
Arthur Prevot <arthrprevot@gmail.com>
2021-09-14 22:22:05 +0200

    udpate pip to pip3 since emr6.x is python3 first.

--- core/scripts/setup_nodes.sh
+++ core/scripts/setup_nodes.sh
@@ -3,8 +3,8 @@
 
 # TODO: reuse "requirement.txt" libs but just the part that need to run on the cluster side
 # Installs below are specific to python version, which is specific to EMR version. TODO: make it independant. Will be fixed when using to emr-6.x since python3 will be default and it will support pip3.
-sudo pip-3.6 install boto3==1.18.41
-sudo pip-3.6 install networkx==2.4
-sudo pip-3.6 install numpy==1.18.5  # need to force this version instead of latest (1.19.2) to be compatible with koalas 1.3.0 (requiring <1.19)
-sudo pip-3.6 install pandas==1.0.0  # downgraded from 1.0.4 to be compatible with koalas. 0.25.1
-sudo pip-3.6 install sqlalchemy==1.3.15  # don't use 1.3.17 since not compatible with sqlalchemy-redshift (0.7.7). See if it is still a pb at https://github.com/sqlalchemy-redshift/sqlalchemy-redshift/issues/195
+sudo pip3 install boto3==1.9.57  # 1.18.41
+sudo pip3 install networkx==2.4
+sudo pip3 install numpy==1.18.5  # need to force this version instead of latest (1.19.2) to be compatible with koalas 1.3.0 (requiring <1.19)
+sudo pip3 install pandas==1.0.0  # downgraded from 1.0.4 to be compatible with koalas. 0.25.1
+sudo pip3 install sqlalchemy==1.3.15  # don't use 1.3.17 since not compatible with sqlalchemy-redshift (0.7.7). See if it is still a pb at https://github.com/sqlalchemy-redshift/sqlalchemy-redshift/issues/195
