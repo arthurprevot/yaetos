633a46db1950aafbb68fa7ba855351c0af204d54
Arthur Prevot <prevota@gmail.com>
2022-03-21 13:44:08 +0100

    Improve extraction job. Works in local but still not from EMR

--- jobs/examples/ex0_extraction_job.py
+++ jobs/examples/ex0_extraction_job.py
@@ -1,13 +1,18 @@
 """ Demo basic extraction job using a public datasource (from wikimedia) """
 from yaetos.etl_utils import ETL_Base, Commandliner
 import requests
+import os
 
 class Job(ETL_Base):
     def transform(self):
         url = self.jargs.api_inputs['path']
         resp = requests.get(url, allow_redirects=True)
-        local_path = 'tmp/tmp_file.csv.gz'
-        open(local_path, 'wb').write(resp.content)  # creating local copy, necessary for sc_sql.read.csv
+        self.logger.info('Finished reading file from {}.'.format(url))
+        tmp_dir = 'file://{}/tmp'.format(os.getcwd())
+        os.makedirs(tmp_dir, exist_ok = True)
+        local_path = tmp_dir+'/tmp_file.csv.gz'
+        open(local_path, 'wb').write(resp.content)  # creating local copy, necessary for sc_sql.read.csv, TODO: check to remove local copy step.
+        self.logger.info('Copied file locally at {}.'.format(local_path))
         sdf = self.sc_sql.read.csv(local_path, header=True)
         return sdf.repartition(1)
 
