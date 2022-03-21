0ed0ffe272a74d899e958e98c7358f107b1dfd3a
Arthur Prevot <prevota@gmail.com>
2022-03-21 14:48:13 +0100

    cleaning

--- jobs/examples/ex0_extraction_job.py
+++ jobs/examples/ex0_extraction_job.py
@@ -1,5 +1,5 @@
 """ Demo basic extraction job using a public datasource (from wikimedia) """
-from yaetos.etl_utils import ETL_Base, Commandliner, FS_Ops_Dispatcher
+from yaetos.etl_utils import ETL_Base, Commandliner
 import requests
 import os
 import pandas as pd
@@ -12,17 +12,13 @@ class Job(ETL_Base):
 
         # Save to local
         tmp_dir = 'tmp'
-        # tmp_dir = 'file://{}/{}'.format(os.getcwd(), tmp_dir)
         os.makedirs(tmp_dir, exist_ok = True)
         local_path = tmp_dir+'/tmp_file.csv.gz'
         open(local_path, 'wb').write(resp.content)  # creating local copy, necessary for sc_sql.read.csv, TODO: check to remove local copy step.
-        # FS_Ops_Dispatcher().save_file(local_path, resp.content, self.jargs.storage)
-        # hadoop fs -put localfile /user/hadoop/hadoopfile.csv)
         self.logger.info('Copied file locally at {}.'.format(local_path))
 
         # Save as dataframe
         pdf = pd.read_csv(local_path)
-        # sdf = self.sc_sql.read.csv(local_path, header=True)
         sdf = self.sc_sql.createDataFrame(pdf)
         return sdf.repartition(1)
 
