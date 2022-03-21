eab5efcd5cb30e6f3aed788d151cb7063c10b30f
Arthur Prevot <prevota@gmail.com>
2022-03-21 23:08:53 +0100

    Move ref to v0.9.4, not pushed to pypi yet

--- scripts/pipify.sh
+++ scripts/pipify.sh
@@ -1,4 +1,4 @@
-# Before executing, version needs to be incremented in setup.py
+# Before executing, version needs to be incremented in ./setup.py and in ./yaetos/scripts/Dockerfile_external.
 # For testing purpose,
 # - option 1: send package to testing pypi server: https://packaging.python.org/guides/using-testpypi/
 # - option 2: "cd (1 level above repo); pip install ./yaetos" will install directly without going through server. Will install shims "yaetos_install"
