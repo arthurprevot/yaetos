8326f09bd58808e42767d68c74c7b4ad95718355
Arthur Prevot <prevota@gmail.com>
2022-03-22 12:52:02 +0100

    fix --chain_dependencies default + comments

--- scripts/pipify.sh
+++ scripts/pipify.sh
@@ -1,4 +1,6 @@
 # Before executing, version needs to be incremented in ./setup.py and in ./yaetos/scripts/Dockerfile_external.
+# To see latest published version https://pypi.org/project/yaetos/
+# To see local version : pip show yaetos
 # For testing purpose,
 # - option 1: send package to testing pypi server: https://packaging.python.org/guides/using-testpypi/
 # - option 2: "cd (1 level above repo); pip install ./yaetos" will install directly without going through server. Will install shims "yaetos_install"
@@ -6,6 +8,7 @@
 # Copy files needed in ./yaetos/scripts/ to check later that they are in git.
 # They need to be also in sync with list in yaetos/scripts/install_env.py
 # TODO: find better way to deal with this, avoiding file duplication
+# TODO: put duplicated files in subfolder "copy" to make it explicit
 cp conf/connections.cfg.example yaetos/scripts/connections.cfg.example
 cp conf/aws_config.cfg.example yaetos/scripts/aws_config.cfg.example
 cp .github/workflows/pythonapp.yml yaetos/scripts/github_pythonapp.yml
