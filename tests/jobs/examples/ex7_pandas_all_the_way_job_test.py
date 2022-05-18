e4dfe66f5c91e1bb5b1636dc05d5f98f5929f6df
Arthur Prevot <prevota@gmail.com>
2022-05-18 19:46:50 +0200

    clean + remove sc loading, much faster.

--- tests/jobs/examples/ex7_pandas_all_the_way_job_test.py
+++ tests/jobs/examples/ex7_pandas_all_the_way_job_test.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class Test_Job(object):
-    def test_transform(self, sc, sc_sql, ss, get_pre_jargs):
+    def test_transform(self, get_pre_jargs):
         some_events = pd.DataFrame([
             {'session_id': 1234, 'action': 'searchResultPage', 'n_results': 1},
             {'session_id': 1234, 'action': 'searchResultPage', 'n_results': 1},
@@ -25,6 +25,5 @@ class Test_Job(object):
             ]
 
         loaded_inputs={'some_events': some_events, 'other_events':other_events}
-        # actual = Job(pre_jargs=get_pre_jargs(loaded_inputs.keys())).etl_no_io(sc=None, sc_sql=None, loaded_inputs=loaded_inputs)[0].toPandas().to_dict(orient='records')
         actual = Job(pre_jargs=get_pre_jargs(loaded_inputs.keys())).etl_no_io(sc=None, sc_sql=None, loaded_inputs=loaded_inputs)[0].to_dict(orient='records')
         assert actual == expected
