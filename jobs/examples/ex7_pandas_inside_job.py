311510815d5e6a6ae2c7a84cccb1af7331665671
Arthur Prevot <prevota@gmail.com>
2022-05-19 10:37:08 +0200

    cleaning

--- jobs/examples/ex7_pandas_inside_job.py
+++ jobs/examples/ex7_pandas_inside_job.py
@@ -12,10 +12,11 @@ class Job(ETL_Base):
         'count_events': types.INT()}
 
     def transform(self, some_events, other_events):
+        # Conversions of inputs to pandas
         some_events_pd = some_events.toPandas()
         other_events_pd = other_events.toPandas()
 
-        # All in pandas
+        # Transformation in pandas
         some_events_pd = some_events_pd[:1000]
         df1 = some_events_pd[some_events_pd.apply(lambda row: row['action'] == 'searchResultPage' and float(row['n_results'])>0, axis=1)]
         df2 = pd.merge(left=df1, right=other_events_pd, how='inner', left_on='session_id', right_on='session_id', indicator = True, suffixes=('_1', '_2'))
@@ -25,7 +26,7 @@ class Job(ETL_Base):
         df3.reset_index(drop=False, inplace=True)
         self.logger.info('Post filter length: {}'.format(len(df1)))
 
-        # Back to Spark
+        # Conversion of output to Spark
         sdf = pdf_to_sdf(df3, self.OUTPUT_TYPES, self.sc, self.sc_sql)
         return sdf
 
--- yaetos/pandas_utils.py
+++ yaetos/pandas_utils.py
@@ -7,18 +7,14 @@ from pathlib import Path
 from yaetos.logger import setup_logging
 logger = setup_logging('pandas')
 
-def create_subfolders(path):
-    """Create subfolders if needed. Can take a path with a file."""
-    # output_file = 'my_file.csv'
-    # path_without_leaf = path.split('/')
-    # path_without_leaf = path_without_leaf[:-1]
-    output_dir = Path(path).parent
-    # import ipdb; ipdb.set_trace()
 
+def create_subfolders(path):
+    """Creates subfolders if needed. Can take a path with a file."""
+    output_dir = Path(path).parent
     output_dir.mkdir(parents=True, exist_ok=True)
-    # df.to_csv(output_dir / output_file)
 
 def save_pandas(df, path):
+    # Only works in local. TODO: add ability to push to S3
     create_subfolders(path)
     df.to_csv(path)
 
