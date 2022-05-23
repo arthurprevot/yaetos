0c0fbcab5604f72771913793ef45b61942e03e71
Arthur Prevot <prevota@gmail.com>
2022-05-23 13:03:27 +0200

    comment

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
-long_description = '\n'.join(long_description.split('\n')[3:])  # To remove header image that doesn't load in pypi.
+long_description = '\n'.join(long_description.split('\n')[3:])  # To remove header image that doesn't load in pypi. TODO: remove need for hardcoding '3'.
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
