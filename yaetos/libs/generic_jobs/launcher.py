fe032ff966da315375a72afa4c6f5537ec960f1e
Arthur Prevot <prevota@gmail.com>
2022-05-23 11:34:24 +0200

    Updates found by running pypify.sh . No need to republish to 0.9.14 yet since minor

--- yaetos/libs/generic_jobs/launcher.py
+++ yaetos/libs/generic_jobs/launcher.py
@@ -1,3 +1,8 @@
+"""
+File needed to run jobs from the yaml manifest (jobs_metadata.yml)
+Usage: $ python jobs/generic/launcher.py --job_name=some_job_name_from_manifest
+"""
+
 from yaetos.etl_utils import Commandliner
 
 Commandliner(Job=None, launcher_file = 'jobs/generic/launcher.py')
