319d6b0334f03d39fb69ff27907fb8372ffa0516
Arthur Prevot <prevota@gmail.com>
2022-05-25 17:08:00 +0200

    make excel loading available in pandas directly

--- yaetos/excel_utils.py
+++ yaetos/excel_utils.py
@@ -6,6 +6,11 @@ logger = setup_logging('Job')
 
 
 def load_excel(jargs, input_name, output_types, sc, sc_sql, **xls_args):
+    pdf = load_excel_pandas(jargs, input_name, **xls_args)
+    sdf = pdf_to_sdf(pdf, output_types, sc, sc_sql)
+    return sdf
+
+def load_excel_pandas(jargs, input_name, **xls_args):
 
     path = jargs.inputs[input_name]['path']
     path = path.replace('s3://', 's3a://') if jargs.mode == 'dev_local' else path
@@ -14,5 +19,4 @@ def load_excel(jargs, input_name, output_types, sc, sc_sql, **xls_args):
     logger.info("Input '{}' loaded from files '{}'.".format(input_name, path))
 
     pdf = pd.read_excel(io=path, engine='openpyxl', **xls_args)
-    sdf = pdf_to_sdf(pdf, output_types, sc, sc_sql)
-    return sdf
+    return pdf
