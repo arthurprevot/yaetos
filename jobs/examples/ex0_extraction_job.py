4f62bdd2957a690105f4f71e24a121fd104566ae
AlejandroUPC <amartinezotal@gmail.com>
2022-05-28 12:24:53 +0200

    :wrench: refactor ex0

--- jobs/examples/ex0_extraction_job.py
+++ jobs/examples/ex0_extraction_job.py
@@ -3,20 +3,22 @@ from yaetos.etl_utils import ETL_Base, Commandliner
 import requests
 import os
 import pandas as pd
+from pyspark import sql
 
 
 class Job(ETL_Base):
-    def transform(self):
-        url = self.jargs.api_inputs['path']
+    def transform(self) -> sql.DataFrame:
+        url = self.jargs.api_inputs["path"]
         resp = requests.get(url, allow_redirects=True)
-        self.logger.info('Finished reading file from {}.'.format(url))
+        self.logger.info("Finished reading file from {}.".format(url))
 
         # Save to local
-        tmp_dir = 'tmp'
-        os.makedirs(tmp_dir, exist_ok = True)
-        local_path = tmp_dir+'/tmp_file.csv.gz'
-        open(local_path, 'wb').write(resp.content)  # creating local copy, necessary for sc_sql.read.csv, TODO: check to remove local copy step.
-        self.logger.info('Copied file locally at {}.'.format(local_path))
+        os.makedirs(tmp_dir, exist_ok=True)
+        local_path = "tmp/tmp_file.csv.gz"
+        with open(local_path, "wb") as lp:
+            lp.write(resp.content)
+        # creating local copy, necessary for sc_sql.read.csv, TODO: check to remove local copy step.
+        self.logger.info(f"Copied file locally at {local_path}.")
 
         # Save as dataframe
         pdf = pd.read_csv(local_path)
@@ -25,5 +27,5 @@ class Job(ETL_Base):
 
 
 if __name__ == "__main__":
-    args = {'job_param_file': 'conf/jobs_metadata.yml'}
+    args = {"job_param_file": "conf/jobs_metadata.yml"}
     Commandliner(Job, **args)
