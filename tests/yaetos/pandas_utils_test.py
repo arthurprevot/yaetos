e99859bee2647eb6a40b1f85227ea605d29fafc5
Arthur Prevot <prevota@gmail.com>
2022-06-02 09:07:16 +0200

    make test more robust. Failed before because of order of loading .csv files

--- tests/yaetos/pandas_utils_test.py
+++ tests/yaetos/pandas_utils_test.py
@@ -21,7 +21,7 @@ def test_load_csvs():
 
     # Test single file option
     path = 'tests/fixtures/data_sample/wiki_example/input/part1.csv'
-    actual = load_csvs(path, read_kwargs={})
+    actual = load_csvs(path, read_kwargs={}).sort_values('uuid')
     expected = pd.DataFrame([
         {'uuid': 'u1', 'timestamp': 2.0, 'session_id': 's1', 'group': 'g1', 'action': 'searchResultPage', 'checkin': np.nan, 'page_id': 'p1', 'n_results': 5.0, 'result_position': np.nan},
         {'uuid': 'u2', 'timestamp': 2.0, 'session_id': 's2', 'group': 'g2', 'action': 'searchResultPage', 'checkin': np.nan, 'page_id': 'p2', 'n_results': 9.0, 'result_position': np.nan},
@@ -33,7 +33,7 @@ def test_load_csvs():
 def test_load_df():
     # Test multiple file option
     path = 'tests/fixtures/data_sample/wiki_example/input/'
-    actual = load_df(path, file_type='csv', read_func='read_csv', read_kwargs={})
+    actual = load_df(path, file_type='csv', read_func='read_csv', read_kwargs={}).sort_values('uuid')
     expected = pd.DataFrame([
         {'uuid': 'u1', 'timestamp': 2.0, 'session_id': 's1', 'group': 'g1', 'action': 'searchResultPage', 'checkin': np.nan, 'page_id': 'p1', 'n_results': 5.0, 'result_position': np.nan},
         {'uuid': 'u2', 'timestamp': 2.0, 'session_id': 's2', 'group': 'g2', 'action': 'searchResultPage', 'checkin': np.nan, 'page_id': 'p2', 'n_results': 9.0, 'result_position': np.nan},
