93421a2d8ecc45dd01391dec2dc1c8ee1f87b8fd
Arthur Prevot <prevota@gmail.com>
2022-06-29 23:57:59 +0200

    more test

--- tests/yaetos/deploy_test.py
+++ tests/yaetos/deploy_test.py
@@ -1,4 +1,6 @@
 from yaetos.deploy import DeployPySparkScriptOnAws as Dep
+import os
+from pathlib import Path as Pt
 
 
 class Test_DeployPySparkScriptOnAws(object):
@@ -8,7 +10,7 @@ class Test_DeployPySparkScriptOnAws(object):
         user = 'n/a'
         actual = Dep.generate_pipeline_name(mode, job_name, user)
         expected = 'yaetos__dev__jobs_d_some_folder_d_job__20220629T205103'
-        assert actual[:-15] == expected[:-15]
+        assert actual[:-15] == expected[:-15]  # [:-15] to remove timestamp
 
     def test_get_job_name(self):
         pipeline_name = 'yaetos__dev__jobs_d_some_folder_d_job__20220629T205103'
@@ -17,15 +19,22 @@ class Test_DeployPySparkScriptOnAws(object):
         assert actual == expected
 
     def test_get_job_log_path_prod(self, deploy_args, app_args):
-        deploy_args = {'mode': 'prod_EMR', **deploy_args}
+        deploy_args['mode'] = 'prod_EMR'
         dep = Dep(deploy_args, app_args)
         actual = dep.get_job_log_path()
         expected = 'pipelines_metadata/jobs_code/production'
         assert actual == expected
 
     def test_get_job_log_path_dev(self, deploy_args, app_args):
-        deploy_args = {'mode': 'dev_EMR', **deploy_args}
+        deploy_args['mode'] = 'dev_EMR'
         dep = Dep(deploy_args, app_args)
         actual = dep.get_job_log_path()
         expected = 'pipelines_metadata/jobs_code/yaetos__dev__some_job_name__20220629T211146'
-        assert actual[:-15] == expected[:-15]
+        assert actual[:-15] == expected[:-15]  # [:-15] to remove timestamp
+
+    def test_get_package_path(self, deploy_args, app_args):
+        app_args['code_source'] = 'repo'
+        dep = Dep(deploy_args, app_args)
+        actual = dep.get_package_path()
+        expected = Pt(os.environ.get('YAETOS_FRAMEWORK_HOME', ''))
+        assert actual == expected
