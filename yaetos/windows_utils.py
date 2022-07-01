d0ee8f00cff2001bab4262ea7711f5d99b90204c
Arthur Prevot <prevota@gmail.com>
2022-07-01 20:31:15 +0200

    cleaning

--- yaetos/windows_utils.py
+++ yaetos/windows_utils.py
@@ -1,17 +1,10 @@
-from yaetos.logger import setup_logging
-logger = setup_logging('Job')
-
 # replacement strings
 WINDOWS_LINE_ENDING = b'\r\n'
 UNIX_LINE_ENDING = b'\n'
 
 def convert_to_linux_eol(path_in, path_out):
-    """
-    path_in can be absolute or relative, same for path_out.
+    """path_in can be absolute or relative, same for path_out.
     path_out can be the same as path_in, i.e. replacing in place."""
-    # # relative or absolute file path, e.g.:
-    # file_path = in_path
-
     with open(path_in, 'rb') as open_file:
         content = open_file.read()
 
