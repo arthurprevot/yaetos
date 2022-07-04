ff9b16be264be328782750060dc8fc2f57e840be
Arthur Prevot <prevota@gmail.com>
2022-07-04 17:10:01 +0200

    centralize functions

--- jobs/marketing/github_contributors_extraction_job.py
+++ jobs/marketing/github_contributors_extraction_job.py
@@ -1,8 +1,12 @@
 from yaetos.etl_utils import ETL_Base, Commandliner
-import requests
-import pandas as pd
 from yaetos.env_dispatchers import Cred_Ops_Dispatcher
+from jobs.marketing.github_utils import pull_all_pages
+# import requests
+import pandas as pd
 import time
+# TODO:
+# - add org associated to contributor (first org) and number of orgs if >1, same for subscriptions, saame for number of followers
+# - move to graphQL
 
 
 class Job(ETL_Base):
@@ -11,11 +15,15 @@ class Job(ETL_Base):
         creds = Cred_Ops_Dispatcher().retrieve_secrets(self.jargs.storage, aws_creds='yaetos/connections', local_creds=self.jargs.connection_file)
         token = creds.get(creds_section, 'token')
         headers = {'Authorization': "Token " + token}
+        # logger = self.logger
 
         data = []
-        for row in repos.iterrows():
+        # for row in repos.iterrows():
+        for row in list(repos.iterrows())[:10]:
             self.logger.info(f"About to pull from repo {row[1]['full_name']}")
-            repo_contribs = self.get_contributors(row[1]['owner'], row[1]['name'], headers)
+            url = f"https://api.github.com/repos/{row[1]['owner']}/{row[1]['name']}/contributors?per_page=100"  # TODO: check stats/contributors instead of contributors
+            #$ repo_contribs = self.get_contributors(row[1]['owner'], row[1]['name'], headers)
+            repo_contribs = pull_all_pages(url, headers)
             repo_contribs = [{**item, 'repo_name': row[1]['full_name']} for item in repo_contribs]
             data.extend(repo_contribs)
             self.logger.info(f"Finished pulling all contributors in {row[1]['full_name']}")
@@ -24,30 +32,30 @@ class Job(ETL_Base):
         keep = ['login', 'id', 'node_id', 'avatar_url', 'html_url', 'organizations_url', 'type', 'site_admin', 'contributions', 'repo_name']
         return df[keep]
 
-    def get_contributors(self, owner, repo, headers):
-        contribs = []
-        url = f"https://api.github.com/repos/{owner}/{repo}/contributors?per_page=100"  # TODO: check stats/contributors instead of contributors
-        resp, data = self.pull_data(url, headers)
-        contribs = data.copy() if resp else []
-
-        while resp and 'next' in resp.links:
-            next_url = resp.links['next']['url']
-            resp, data = self.pull_data(next_url, headers)
-            if resp:
-                contribs.extend(data)
-            time.sleep(1. / 4999.)  # i.e. 5000 requests max / sec
-        return contribs
-
-    def pull_data(self, url, headers):
-        try:
-            resp = requests.get(url, headers=headers)
-            data = resp.json()
-            self.logger.info(f"pulling from {url}")
-        except Exception:
-            resp = None
-            data = None
-            self.logger.info(f"Couldn't pull data from {url}")
-        return resp, data
+    # def get_contributors(self, owner, repo, headers):
+    #     contribs = []
+    #     url = f"https://api.github.com/repos/{owner}/{repo}/contributors?per_page=100"  # TODO: check stats/contributors instead of contributors
+    #     resp, data = self.pull_data(url, headers)
+    #     contribs = data.copy() if resp else []
+    #
+    #     while resp and 'next' in resp.links:
+    #         next_url = resp.links['next']['url']
+    #         resp, data = self.pull_data(next_url, headers)
+    #         if resp:
+    #             contribs.extend(data)
+    #         time.sleep(1. / 4999.)  # i.e. 5000 requests max / sec
+    #     return contribs
+    #
+    # def pull_data(self, url, headers):
+    #     try:
+    #         resp = requests.get(url, headers=headers)
+    #         data = resp.json()
+    #         self.logger.info(f"pulling from {url}")
+    #     except Exception:
+    #         resp = None
+    #         data = None
+    #         self.logger.info(f"Couldn't pull data from {url}")
+    #     return resp, data
 
 
 if __name__ == "__main__":
