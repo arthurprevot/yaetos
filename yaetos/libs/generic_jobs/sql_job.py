e5ea64a3849af06d5dcacc8bd28c2ef250e36712
Arthur Prevot <prevota@gmail.com>
2022-07-10 13:20:50 -0500

    Fix generic sql_job so it works in pipeline

--- yaetos/libs/generic_jobs/sql_job.py
+++ yaetos/libs/generic_jobs/sql_job.py
@@ -1,4 +1,6 @@
 from yaetos.etl_utils import Commandliner
-from yaetos.sql_job import Job as SqlJob
+from yaetos.sql_job import Job
 
-Commandliner(SqlJob)
+
+if __name__ == "__main__":
+    Commandliner(Job)
