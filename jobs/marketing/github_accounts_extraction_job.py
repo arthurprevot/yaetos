99e8df5e1c5445d369b05e17361fa33798cf9ffb
Arthur Prevot <prevota@gmail.com>
2022-07-10 13:42:27 -0500

    cleaning

--- jobs/marketing/github_accounts_extraction_job.py
+++ jobs/marketing/github_accounts_extraction_job.py
@@ -10,18 +10,13 @@ class Job(ETL_Base):
         creds = Cred_Ops_Dispatcher().retrieve_secrets(self.jargs.storage, aws_creds='yaetos/connections', local_creds=self.jargs.connection_file)
         token = creds.get(creds_section, 'token')
         headers = {'Authorization': "Token " + token}
+        github_accounts_man = github_accounts_man[:3]
 
         data = []
-        # for owner in github_accounts_man['account_name'].tolist():
         for ii, row in github_accounts_man.iterrows():
             self.logger.info(f"About to pull from owner {row['account_name']}")
             url = f"https://api.github.com/users/{row['account_name']}"
-
-            # accounts_info = self.get_accounts_info(owner, headers)
-            # accounts_info = pull_all_pages(url, headers)
             resp, data_line = pull_1page(url, headers)
-            # import ipdb; ipdb.set_trace()
-
             if resp:
                 data_line = {**row, **data_line}
                 data.append(data_line)
@@ -29,15 +24,6 @@ class Job(ETL_Base):
         df = pd.DataFrame(data)
         self.logger.info(f"Fields {df.columns}")
         return df
-
-    # @staticmethod
-    # def get_accounts_info(owner, headers):
-    #     url = f"https://api.github.com/users/{owner}"
-    #     request = requests.get(url, headers=headers)
-    #     if request.status_code == 200:
-    #         return request.json()
-    #     else:
-    #         return None
 
 
 if __name__ == "__main__":
--- jobs/marketing/github_repos_extraction_job.py
+++ jobs/marketing/github_repos_extraction_job.py
@@ -11,16 +11,11 @@ class Job(ETL_Base):
         token = creds.get(creds_section, 'token')
         headers = {'Authorization': "Token " + token}
 
-        # github_accounts = github_accounts[:3]
         data = []
         for ii, row in github_accounts.iterrows():
             self.logger.info(f"About to pull from owner {row['account_name']}")
-
-            url = f"https://api.github.com/users/{row['account_name']}/repos"
-            # repos_owner = self.get_repos(row['account_name'], row['public_repos'], headers)
+            url = f"https://api.github.com/users/{row['account_name']}/repos"  # per_page=100
             repos_owner = pull_all_pages(url, headers)
-            import ipdb; ipdb.set_trace()
-
             repos_owner = [{**subrow, 'owner': row['account_name']} for subrow in repos_owner]
             data.extend(repos_owner)
             self.logger.info(f"Finished pulling all repos in {row['account_name']}")
@@ -28,22 +23,6 @@ class Job(ETL_Base):
         self.logger.info(f"Fields {df.columns}")
         keep = ['id', 'node_id', 'name', 'full_name', 'private', 'owner', 'html_url', 'description', 'created_at', 'updated_at', 'pushed_at', 'homepage', 'size', 'stargazers_count', 'watchers_count', 'forks_count', 'language', 'watchers']
         return df[keep]
-
-    # def get_repos(self, owner, repo_count, headers):
-    #     import time
-    #     import requests
-    #     repos = []
-    #     pages = repo_count // 100 + 1
-    #     for page_num in range(1, pages + 1):  # +1 to make it inclusive.
-    #         url = f"https://api.github.com/users/{owner}/repos?page={page_num}"
-    #         try:
-    #             repo = requests.get(url, headers=headers).json()
-    #             repos.extend(repo)
-    #             self.logger.info(f"###pulling from page {page_num}")
-    #         except Exception as ex:
-    #             self.logger.info(f"###Couldn't pull data from {url} with {ex}")
-    #         time.sleep(1. / 4999.)  # i.e. 5000 requests max / sec
-    #     return repos
 
 
 if __name__ == "__main__":
