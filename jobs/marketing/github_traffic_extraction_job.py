5726fe69c2e203bda24f1ea366f91147df3e04e7
Arthur Prevot <prevota@gmail.com>
2022-07-11 09:50:25 -0500

    remove hardcoded repo name and owner. Now pulled from input table. Allows tracking several repos.

--- jobs/marketing/github_traffic_extraction_job.py
+++ jobs/marketing/github_traffic_extraction_job.py
@@ -6,7 +6,7 @@ from jobs.marketing.github_utils import pull_all_pages
 
 
 class Job(ETL_Base):
-    def transform(self):
+    def transform(self, my_repos):
         creds_section = self.jargs.api_inputs['creds']
         creds = Cred_Ops_Dispatcher().retrieve_secrets(self.jargs.storage, aws_creds='yaetos/connections', local_creds=self.jargs.connection_file)
         token = creds.get(creds_section, 'token')
@@ -15,17 +15,19 @@ class Job(ETL_Base):
         data = []
         self.logger.info(f"About to pull data")
 
-        url = f"https://api.github.com/repos/arthurprevot/yaetos/traffic/clones"
-        clones = pull_all_pages(url, headers)
-        clones_ts = clones[0]['clones']
-        clones_ts = [{**item, 'category': 'clones'} for item in clones_ts]
-        data.extend(clones_ts)
+        for ii, row in my_repos.iterrows():
+            self.logger.info(f"About to pull contributors from repo {row['owner']}-{row['repo']}")
+            url = f"https://api.github.com/repos/{row['owner']}/{row['repo']}/traffic/clones"
+            clones = pull_all_pages(url, headers)
+            clones_ts = clones[0]['clones']
+            clones_ts = [{**item, 'category': 'clones', 'owner': row['owner'], 'repo': row['repo']} for item in clones_ts]
+            data.extend(clones_ts)
 
-        url = f"https://api.github.com/repos/arthurprevot/yaetos/traffic/views"
-        views = pull_all_pages(url, headers)
-        views_ts = views[0]['views']
-        views_ts = [{**item, 'category': 'views'} for item in views_ts]
-        data.extend(views_ts)
+            url = f"https://api.github.com/repos/{row['owner']}/{row['repo']}/traffic/views"
+            views = pull_all_pages(url, headers)
+            views_ts = views[0]['views']
+            views_ts = [{**item, 'category': 'views', 'owner': row['owner'], 'repo': row['repo']} for item in views_ts]
+            data.extend(views_ts)
 
         self.logger.info(f"Finished pulling data")
         df = pd.DataFrame(data)
