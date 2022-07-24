86e90d724eddfcd16b940af26843758ad7ba5e18
Arthur Prevot <prevota@gmail.com>
2022-07-24 10:37:50 -0500

    better merge

--- jobs/marketing/people_merge_job.py
+++ jobs/marketing/people_merge_job.py
@@ -12,14 +12,27 @@ class Job(ETL_Base):
                     `Email Address` as email_address,
                     `First Name` as first_name_lk,
                     `Last Name` as last_name_lk,
-                    Position
+                    Position,
+                    'linkedin' as source_lk
                 FROM linkedin
             ),
             perso_mod as (
-                SELECT concat(first_name, ' ', ` last_name`) as ps_id, *
+                SELECT
+                    concat(first_name, ' ', coalesce(last_name, '')) as ps_id,
+                    'perso' as source_ps,
+                    *
                 FROM perso
             )
-            SELECT coalesce(lk.lk_id, ps.ps_id) as id, lk.*, ps.*
+            SELECT
+              coalesce(lk.lk_id, ps.ps_id) as id,
+              case
+                when source_lk is not NULL and source_ps is not NULL THEN 'Both'
+                when source_lk is not NULL THEN source_lk
+                when ps.source_ps is not NULL THEN source_ps
+                else NULL
+              end as source,
+              lk.*,
+              ps.*
             FROM linkedin_mod lk
             FULL OUTER JOIN perso_mod as ps on ps.ps_id=lk.lk_id
             order by 1
