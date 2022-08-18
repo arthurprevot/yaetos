8b21db420b13431195ff7c4ee14745b71f0c4db3
Arthur Prevot <prevota@gmail.com>
2022-08-18 16:22:57 +0200

    Adding ability to put job params in sql file and start as is

--- yaetos/sql_job.py
+++ yaetos/sql_job.py
@@ -5,10 +5,15 @@ class Job(ETL_Base):
     """To run/deploy sql jobs, using --sql_file arg."""
 
     def set_jargs(self, pre_jargs, loaded_inputs={}):
-        # Function called only if running the job directly, i.e. "python yaetos/sql_job.py --sql_file=jobs/some_job.sql", ignored if running from "python jobs/generic/launcher.py --job_name=some_job.sql"
+        # Function called only if running the job directly, i.e. "python jobs/generic/sql_job.py --sql_file=jobs/some_job.sql", ignored if running from "python jobs/generic/launcher.py --job_name=some_job.sql"
         sql_file = pre_jargs['cmd_args']['sql_file']
         job_name = Job_Yml_Parser.set_job_name_from_file(sql_file)
         pre_jargs['job_args']['job_name'] = job_name
+
+        sql = self.read_sql_file(sql_file)
+        job_args = self.get_params_from_sql(sql)
+        pre_jargs['job_args'].update(job_args)
+
         return Job_Args_Parser(defaults_args=pre_jargs['defaults_args'], yml_args=None, job_args=pre_jargs['job_args'], cmd_args=pre_jargs['cmd_args'], loaded_inputs=loaded_inputs)
 
     def transform(self, **ignored):
@@ -24,6 +29,12 @@ class Job(ETL_Base):
         sql = fh.read()
         fh.close()
         return sql
+
+    @staticmethod
+    def get_params_from_sql(sql):
+        param_lines = [item.split('-----')[1].split(':') for item in sql.split('\n') if item.startswith('----- ')]
+        params = {eval(key): eval(value) for key, value in param_lines}
+        return params
 
 
 if __name__ == "__main__":
