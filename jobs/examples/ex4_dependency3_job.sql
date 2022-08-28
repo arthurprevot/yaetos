454dcee02a1f85bfde1a113cf8ca064d56f8e4d4
Arthur Prevot <prevota@gmail.com>
2022-08-28 14:56:56 +0200

    move sql job from sample dependency pipeline to pandas instead of spark. Works locally.

--- jobs/examples/ex4_dependency3_job.sql
+++ jobs/examples/ex4_dependency3_job.sql
@@ -1,5 +1,5 @@
 -- Path of 'some_events' and 'other_events' tables can be found in conf/jobs_metadata.yml
--- to run: python yaetos/sql_spark_job.py --sql_file=jobs/examples/ex4_dependency3_job.sql
+-- to run: python yaetos/sql_pandas_job.py --sql_file=jobs/examples/ex4_dependency3_job.sql
 
 
 SELECT se.session_id, session_length, doubled_length, session_length*4 as quadrupled_length
