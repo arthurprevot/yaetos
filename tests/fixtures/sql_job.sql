0eedd0edc74ad84c3ac7845980de3f981a0a3c7d
Arthur Prevot <prevota@gmail.com>
2022-08-28 15:54:47 +0200

    fix 1 test

--- tests/fixtures/sql_job.sql
+++ tests/fixtures/sql_job.sql
@@ -1,4 +1,4 @@
----param--- 'repartition' : 2 ---
+----param---- 'some_sql_file_param' : 'some_value' ----
 
 SELECT *
 FROM some_events
--- tests/yaetos/sql_job_test.py
+++ tests/yaetos/sql_job_test.py
@@ -1,9 +1,9 @@
-from yaetos.sql_job import Job
+from yaetos.sql_spark_job import Job
 import textwrap
 
 
 class Test_Job(object):
-    def test_set_jargs(self, sc, sc_sql, ss, get_pre_jargs):
+    def test_set_jargs(self, get_pre_jargs):
         loaded_inputs = {}
         pre_jargs = get_pre_jargs(input_names=loaded_inputs.keys())
         pre_jargs['cmd_args']['sql_file'] = 'tests/fixtures/sql_job.sql'
@@ -11,7 +11,7 @@ class Test_Job(object):
         actual = job.set_jargs(pre_jargs, loaded_inputs)
 
         expected = {
-            'repartition': 2,
+            'some_sql_file_param': 'some_value',
             'manage_git_info': False,
             'mode': 'dev_local',
             'deploy': 'none',
@@ -26,9 +26,9 @@ class Test_Job(object):
 
     def test_get_params_from_sql(self):
         sql = textwrap.dedent("""
-        ---param--- 'param_a': 'value_a' ---
-        ---param--- 'param_to_be_ignored': 'some_value' --
-        --param--- 'param_to_be_ignored': 'some_value' ---
+        ----param---- 'param_a': 'value_a' ----
+        ----param---- 'param_to_be_ignored': 'some_value' --
+        ---param--- 'param_to_be_ignored': 'some_value' ----
         """)
         expected = {'param_a': 'value_a'}
         actual = Job.get_params_from_sql(sql)
