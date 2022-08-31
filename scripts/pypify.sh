fd5981f320873dc2bd958fc80050abc46ed162cc
Arthur Prevot <prevota@gmail.com>
2022-09-01 01:10:27 +0200

    Changed pypify to account for latest jobs and job_name in pip install lib + executed it to sync files

--- scripts/pypify.sh
+++ scripts/pypify.sh
@@ -11,6 +11,7 @@
 # TODO: put duplicated files in subfolder "copy" to make it explicit
 cp conf/connections.cfg.example yaetos/scripts/copy/connections.cfg.example
 cp conf/aws_config.cfg.example yaetos/scripts/copy/aws_config.cfg.example
+cp yaetos/libs/pytest_utils/conftest.py tests/conftest.py  # yaetos/libs/pytest_utils should be considered master copy and tests/conftest.py should be removed, although currently tests/conftest.py may be changed directly for dev reasons.
 ## Jobs
 cp jobs/generic/copy_job.py yaetos/libs/generic_jobs/copy_job.py
 cp jobs/generic/deployer.py yaetos/libs/generic_jobs/deployer.py
@@ -19,11 +20,13 @@ cp jobs/generic/launcher.py yaetos/libs/generic_jobs/launcher.py
 cp jobs/generic/sql_spark_job.py yaetos/libs/generic_jobs/sql_spark_job.py
 cp jobs/generic/sql_pandas_job.py yaetos/libs/generic_jobs/sql_pandas_job.py
 cp jobs/examples/ex0_extraction_job.py yaetos/scripts/copy/ex0_extraction_job.py
-cp jobs/examples/ex1_frameworked_job.py yaetos/scripts/copy/ex1_frameworked_job.py
 cp jobs/examples/ex1_sql_job.sql yaetos/scripts/copy/ex1_sql_job.sql
-cp jobs/examples/ex7_pandas_job.py yaetos/scripts/copy/ex1_pandas_job.py
-cp tests/jobs/examples/ex1_frameworked_job_test.py yaetos/scripts/copy/ex1_frameworked_job_test.py
-cp tests/jobs/examples/ex1_sql_job_test.py yaetos/scripts/copy/ex1_sql_job_test.py
+cp jobs/examples/ex7_pandas_job.py yaetos/scripts/copy/ex1_pandas_api_job.py
+cp jobs/examples/ex1_frameworked_job.py yaetos/scripts/copy/ex1_spark_api_job.py
+cp tests/jobs/examples/ex1_sql_pandas_job_test.py yaetos/scripts/copy/ex1_sql_pandas_job_test.py
+cp tests/jobs/examples/ex7_pandas_job_test.py yaetos/scripts/copy/ex1_pandas_api_job_test.py
+cp tests/jobs/examples/ex1_sql_spark_job_test.py yaetos/scripts/copy/ex1_sql_spark_job_test.py
+cp tests/jobs/examples/ex1_frameworked_job_test.py yaetos/scripts/copy/ex1_spark_api_job_test.py
 # Other files not to be copied exactly but close:
 #  - cp .github/workflows/pythonapp.yml yaetos/scripts/github_pythonapp.yml
 #  - cp tests/jobs/examples/ex7_pandas_job_test.py yaetos/scripts/copy/ex1_pandas_job_test.py
--- yaetos/libs/pytest_utils/conftest.py
+++ yaetos/libs/pytest_utils/conftest.py
@@ -1,6 +1,9 @@
 import pytest
-from pyspark import SparkContext
-from pyspark.sql import SQLContext, SparkSession
+try:
+    from pyspark import SparkContext
+    from pyspark.sql import SQLContext, SparkSession
+except ModuleNotFoundError or ImportError:
+    print('Yaetos will work in pandas mode only or to push jobs to AWS, since pyspark is not found.')
 
 
 @pytest.fixture(scope="session")
@@ -40,3 +43,15 @@ def get_pre_jargs():
             pre_jargs.update(pre_jargs_over)
         return pre_jargs
     return internal_function
+
+
+@pytest.fixture
+def deploy_args():
+    return {'aws_setup': 'dev',
+            'aws_config_file': 'conf/aws_config.cfg.example',
+            'mode': 'dev_EMR'}
+
+
+@pytest.fixture
+def app_args():
+    return {'py_job': 'some/job.py', 'job_name': 'some_job_name'}
