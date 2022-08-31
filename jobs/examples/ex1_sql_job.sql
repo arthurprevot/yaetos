012e0804e17ad7343fdad3e357426bc6fb1f192a
Arthur Prevot <prevota@gmail.com>
2022-08-31 13:07:14 +0200

    simpler. Tested. Still functional.

--- jobs/examples/ex1_sql_job.sql
+++ jobs/examples/ex1_sql_job.sql
@@ -1,7 +1,6 @@
 -- SQL Query
 -- Job params below (yaetos will parse lines starting with "----param---- " and finishing with "----", like below)
 ----param---- 'job_param_file' : 'conf/jobs_metadata.yml' ----
-----param---- 'job_name' : 'examples/ex1_sql_job.sql' ----
 ----param---- 'param_a' : 'value_a' ----
 ----param---- 'param_b' : 'value_b' ----
 
@@ -15,5 +14,5 @@ order by count(*) desc
 -- Path of 'some_events' and 'other_events' tables can be found in conf/jobs_metadata.yml
 -- to run over spark using job_metadata.yml launcher (implying params above will be ignored):  python jobs/generic/launcher.py --job_name=examples/ex1_sql_job.sql
 -- to run over pandas using job_metadata.yml launcher (implying params above will be ignored): python jobs/generic/launcher.py --job_name=examples/ex1_sql_pandas_job
--- to run over spark using standalone sql launcher:  python jobs/generic/sql_spark_job.py  --sql_file=jobs/examples/ex1_sql_job.sql
--- to run over pandas using standalone sql launcher: python jobs/generic/sql_pandas_job.py --sql_file=jobs/examples/ex1_sql_job.sql --job_name=examples/ex1_sql_pandas_job # overwriting param above. 
+-- to run over spark using standalone sql launcher:  python jobs/generic/sql_spark_job.py  --sql_file=jobs/examples/ex1_sql_job.sql  # i.e. job_name derived from sql_file
+-- to run over pandas using standalone sql launcher: python jobs/generic/sql_pandas_job.py --sql_file=jobs/examples/ex1_sql_job.sql --job_name=examples/ex1_sql_pandas_job
