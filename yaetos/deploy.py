60a8c206003cca10b2ee1cca9e7dc942be4fe1f8
Arthur Prevot <prevota@gmail.com>
2022-09-18 23:11:02 +0200

    rename requirements_alt.txt to requirements_base.txt

--- yaetos/deploy.py
+++ yaetos/deploy.py
@@ -292,7 +292,7 @@ class DeployPySparkScriptOnAws(object):
         for item in ['setup_master.sh',
                      'setup_master_alt.sh',
                      'requirements.txt',
-                     'requirements_alt.txt',
+                     'requirements_base.txt',
                      'setup_nodes.sh',
                      'setup_nodes_alt.sh',
                      'terminate_idle_cluster.sh']:
@@ -333,7 +333,7 @@ class DeployPySparkScriptOnAws(object):
         """
         setup_master = 'setup_master.sh' if self.deploy_args.get('spark_version', '2.4') == '2.4' else 'setup_master_alt.sh'
         setup_nodes = 'setup_nodes.sh' if self.deploy_args.get('spark_version', '2.4') == '2.4' else 'setup_nodes_alt.sh'
-        requirements = 'requirements.txt' if self.deploy_args.get('spark_version', '2.4') == '2.4' else 'requirements_alt.txt'
+        requirements = 'requirements.txt' if self.deploy_args.get('spark_version', '2.4') == '2.4' else 'requirements_base.txt'
 
         # Looping through all 4 steps below doesn't work (Fails silently) so done 1 by 1.
         s3.Object(self.s3_bucket_logs, self.package_path + '/setup_master.sh')\
