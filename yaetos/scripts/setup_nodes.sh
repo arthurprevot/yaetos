151d5fa3504fc51f31e92360ce9ed7a5d94d4486
Arthur Prevot <arthrprevot@gmail.com>
2024-01-10 11:37:12 +0100

    cleaning

--- yaetos/scripts/setup_nodes.sh
+++ yaetos/scripts/setup_nodes.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Script to configure all nodes on EMR cluster. (Setup as part of a boostrap operation in AWS)
 
-# Installs below are specific to python version, which is specific to EMR version. TODO: make it independant. Will be fixed when using to emr-6.x since python3 will be default and it will support pip3.
-
 sudo pip3 install --ignore-installed awscli==1.32.14  # AWS emr7.0.0 has version 2.14.5 installed, but 1.32.14 is latest on pypi !
 sudo pip3 install boto3==1.34.14
 sudo pip3 install networkx==3.1
