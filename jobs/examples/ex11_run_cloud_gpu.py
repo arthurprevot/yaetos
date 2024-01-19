846c09f4be7dc608480213efddd5d5cba3b58298
Arthur Prevot <arthrprevot@gmail.com>
2024-01-19 01:06:07 +0100

    cleaning

--- jobs/examples/ex11_run_cloud_gpu.py
+++ jobs/examples/ex11_run_cloud_gpu.py
@@ -1,11 +1,10 @@
-"""To show a troubleshooting method, using 'import ipdb; ipdb.set_trace()' below."""
+"""To show a job using GPU, for deeplearning in that case. Can also run on GPU in the cloud."""
 from yaetos.etl_utils import ETL_Base, Commandliner
 from transformers import AlbertTokenizer, TFAlbertForSequenceClassification
 from transformers import file_utils
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-
 
 
 class Job(ETL_Base):
@@ -26,19 +25,9 @@ class Job(ETL_Base):
         model = self.load_model()
         predictions = self.predict(model, x_test_proc)
         evaluations = pd.DataFrame({'text': x_test, 'predictions': predictions, 'real': y_test})
-
-        # model = self.finetune_model(x_train_proc, y_train)
-        # path = Path_Handler(self.jargs.output_model['path'], self.jargs.base_path, self.jargs.merged_args.get('root_path')).expand_now(now_dt=self.start_dt)
-        # self.save_model(model, path)
-
-        # evaluations = self.evaluate(model, x_test, y_test, x_train, y_train)
-
-        # # Generate a plot of the model's architecture
-        # tf.keras.utils.plot_model(model, to_file='model_architecture.png', show_shapes=True)
         return evaluations
 
     def load_model(self):
-        # Load model
         model = TFAlbertForSequenceClassification.from_pretrained(self.MODEL_NAME)
         return model
 
@@ -58,34 +47,6 @@ class Job(ETL_Base):
         x = [encoded_inputs['input_ids'], encoded_inputs['attention_mask']]
         return x
 
-    # def finetune_model(self, x_train, y_train):
-    #     # Load model
-    #     model = TFAlbertForSequenceClassification.from_pretrained(self.MODEL_NAME)
-
-    #     # Convert labels to a TensorFlow tensor
-    #     y_train = tf.constant(y_train)
-
-    #     # Define loss function and optimizer
-    #     loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    #     optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
-
-    #     # Compile the model
-    #     model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
-
-    #     # Train the model
-    #     model.fit(x=x_train, y=y_train, batch_size=8, epochs=10)
-    #     self.logger.info(f"Model summary, post finetuning: {model.summary()}")
-
-    #     return model
-
-    # @staticmethod
-    # def save_model(model, path):
-    #     model.save_pretrained(path)
-
-    # @staticmethod
-    # def reload_model(path):
-    #     return TFAlbertForSequenceClassification.from_pretrained(path)
-
     @staticmethod
     def predict(model, x):
         predictions = model.predict(x)
@@ -95,19 +56,6 @@ class Job(ETL_Base):
         # Take the argmax to get the most likely class
         predicted_classes = np.argmax(probabilities, axis=-1)
         return predicted_classes
-
-    # def evaluate(self, model, x_test, y_test, x_train, y_train):
-    #     x_proc = self.preprocess(x_test)
-    #     predictions = self.predict(model, x_proc)
-    #     df_test = pd.DataFrame({'text': x_test, 'predictions': predictions, 'real': y_test})
-    #     df_test['train_or_test'] = 'test'
-
-    #     x_proc = self.preprocess(x_train)
-    #     predictions = self.predict(model, x_proc)
-    #     df_train = pd.DataFrame({'text': x_train, 'predictions': predictions, 'real': y_train})
-    #     df_train['train_or_test'] = 'train'
-
-    #     return pd.concat([df_test, df_train])
 
 
 if __name__ == "__main__":
