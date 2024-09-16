bd0bb2ca194b83f9b1baefdbade4c4e80e2bb80a
Arthur Prevot <arthrprevot@gmail.com>
2024-09-17 00:38:33 +0200

    linting

--- yaetos/pandas_utils.py
+++ yaetos/pandas_utils.py
@@ -124,6 +124,7 @@ def check_pk(df, pks):
         logger.info("Given fields ({}) are PKs (i.e. unique). count=count_pk={}".format(pks, count))
         return True
 
+
 def add_created_at(pdf, start_dt):
     pdf['_created_at'] = start_dt
     return pdf
