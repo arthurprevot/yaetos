16138f222144b20549fcbe5309a247b1b7888113
Arthur Prevot <arthrprevot@gmail.com>
2024-10-14 22:03:12 +0200

    cleaning + comments

--- jobs/generic/multi_region_meta_job.py
+++ jobs/generic/multi_region_meta_job.py
@@ -14,15 +14,8 @@ class Job(ETL_Base):
             args['region'] = region
             args['inputs'] = args['inputs_region']
             args['output'] = args['output_region']
-            # yml_args = Job_Yml_Parser(job_name, jargs_single_region.job_param_file, jargs_single_region.mode).yml_args
-            # jargs = Job_Args_Parser(jargs_single_region.defaults_args, yml_args, jargs_single_region.job_args, jargs_single_region.cmd_args, build_yml_args=False, loaded_inputs={})
-
-            # Job = get_job_class(yml_args['py_job'])
             jargs_single_region = Job_Args_Parser(defaults_args=args, yml_args={}, job_args={}, cmd_args={}, build_yml_args=False, loaded_inputs={})
             job = JobSingleRegion(jargs=jargs_single_region, loaded_inputs={})
-            # job = JobSingleRegion(pre_jargs={'defaults_args': {}, 'job_args': jargs_single_region.merged_args, 'cmd_args': {}}, loaded_inputs={})
-            
-            # import ipdb; ipdb.set_trace()
             dfs[args['job_name']] = job.etl(self.sc, self.sc_sql)
 
         return None
